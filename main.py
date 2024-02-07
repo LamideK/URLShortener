ffcd941d54dce42292428079ab0a34246798cafe
LamideK <119982368+LamideK@users.noreply.github.com>
2024-02-06 21:57:58 -0500

    Updated URLShortener
    - Added Get method to redirect to target URL
    - Bug Fixes
    - Corrected status responses
    - Functional counter for Admin table

--- main.py
+++ main.py
@@ -1,48 +1,64 @@
-from fastapi import FastAPI, HTTPException
-from . import schema
-import validators
-from . import models, schema
+from fastapi import FastAPI, HTTPException, Response, status, Depends, Request
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
-from sqlalchemy.orm import Session
-from . database import engine, get_db
-import psycopg2
-import psycopg
-import psycopg_binary
-from psycopg2.extras import RealDictCursor
-
-
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import update, insert
+import models, schemas, database
+#from models import Urls
+from database import engine
+import validators
+import secrets
+import random
+import string
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get('/')
-async def read_root():
-    return 'Welcome to URL Shortening API'
+clicks = 0
 
+@app.post("/urlshortener/", status_code= status.HTTP_201_CREATED)
+async def create_url(url: schemas.URLBase, db: Session= Depends(database.get_db)): 
 
-def raise_bad_request(message):
-    raise HTTPException(status_code=400, detail=message)
-    # handling bad request if the url is invalid (Leveraging on validators)
-
-
-@app.post(('/url'))
-async def create_url(url: schema.URLBase):
     if not validators.url(url.target_url):
         raise_bad_request(message='The URL is invalid')
-    return f"TODO: Create database entry for: {url.target_url}"
-    # this is a to do for the actual posting if the url is valid
+
+    key = "".join(random.choice(string.ascii_lowercase + string.digits) for _ in range(5))
+    secret_key = "".join(secrets.choice(string.ascii_lowercase + string.digits) for _ in range(8))
+
+    db_url = models.Urls(
+        target_url=url.target_url, changed_url= key, is_active= True
+    )
+
+    clicks_query = db.query(models.AdminData.times_clicked).filter(models.AdminData.target_url==url.target_url)
+    clicks = clicks_query.first()
+    
+    if clicks == None:
+        clicks = 1
+        admin_db =  models.AdminData( 
+        target_url = url.target_url, times_clicked=clicks, secret_url_key= secret_key
+        )
+        db.add(admin_db)
+        detail = f"Succesfully shortened to {key}"
+
+    else:
+        clicks = clicks.times_clicked + 1
+        stmt = update(models.AdminData).values(times_clicked=clicks, target_url = url.target_url, secret_url_key= secret_key).where(models.AdminData.target_url == url.target_url)
+        admin_db =  db.execute(stmt,  execution_options={"synchronize_session": "auto"})
+        detail = f"Succesfully shortened to {key}"
+    
+    db.add(db_url)    
+    db.commit()
+    db.refresh(db_url)
+   
+    return detail
 
 
-@app.get('/{url_key}')
-async def get_url_key():
-    return
-
-
-@app.get('/admin/{secret_key}')
-def get_secret_key():
-    return
-
-@app.delete('/admin/{secret_key}')
-def delete_secretkey():
-    return
+@app.get("/{url_key}", status_code= status.HTTP_307_TEMPORARY_REDIRECT)
+async def redirect_url(url_key: str, request: Request ,db: Session= Depends(database.get_db)):
+    converted_url = db.query(models.Urls).filter(models.Urls.changed_url==url_key).first()
+    if not converted_url:
+        raise HTTPException(status_code=404, detail="url does not exist")
+    else:
+        detail = "Successfully Redirected"
+        return detail
--- models.py
+++ models.py
@@ -1,14 +1,28 @@
-from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.sql.expression import text
-from sqlalchemy.sql.sqltypes import TIMESTAMP
+import database
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+
 
 class Urls(Base):
     __tablename__ = 'url_list'
 
-    id = Column(Integer, primary_key= True, nullable= False)
-    url = Column(String, nullable= False)
-    target_url = Column(String, nullable= False)  # stores the target url
-    admin_url = Column(String, nullable= False)
-    is_active = Column(Boolean, nullable= True) 
+    id = Column(Integer, primary_key= True, nullable= False, index= True)
+    target_url = Column(String, nullable= True)
+    changed_url = Column(String, nullable= True)  # stores the converted target url
+    is_active = Column(Boolean, default= True) 
     clicks = Column(Integer) 
+
+    admin_data = relationship("AdminData", back_populates= "owner")
+
+
+class AdminData(Base):
+    __tablename__ = 'admin_data'
+
+    id = Column(Integer, primary_key= True, nullable= False)
+    target_url = Column(String, ForeignKey("url_list.target_url"), nullable= True)
+    times_clicked = Column(Integer, nullable= False)
+    secret_url_key = Column(String, nullable= False)
+
+    owner = relationship("Urls", back_populates= 'admin_data')
